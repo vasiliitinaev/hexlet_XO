6491d988f937fb1d1c49e38ccc5fc674364b05f4
v.v.tinaev <v.v.tinaev@tinkoff.ru>
2021-02-07 10:44:26 +0300

    FieldTest testSetFigureWhenAlreadyOccupied added - created

--- tst/io/hexlet/xo/model/FieldTest.java
+++ tst/io/hexlet/xo/model/FieldTest.java
@@ -1,5 +1,6 @@
 package io.hexlet.xo.model;
 
+import io.hexlet.xo.model.exceptions.AlreadyOccupiedException;
 import io.hexlet.xo.model.exceptions.InvalidPointException;
 import org.junit.Test;
 
@@ -80,6 +81,19 @@ public class FieldTest {
             field.getFigure(inputPoint);
             fail();
         } catch (final InvalidPointException e) {}
+    }
+
+    @Test
+    public void testSetFigureWhenAlreadyOccupied() throws Exception {
+        final Field field = new Field();
+        final Point inputPoint = new Point(0, 0);
+        final Figure inputFigure = Figure.O;
+
+        field.setFigure(inputPoint, inputFigure);
+        try {
+            field.setFigure(inputPoint, inputFigure);
+            fail();
+        } catch (final AlreadyOccupiedException e) {}
     }
 
 }
